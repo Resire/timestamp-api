6ae2304f005fe5c663d1b7c05c936273324b75e1
Jeff <jakerns8379@gmail.com>
2017-03-16 08:44:49 +0000

    filename correction

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+"use strict"
+
 var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Timestamp Microservice')
 })
 
 app.listen(8080, function () {
