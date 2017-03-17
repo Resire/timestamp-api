561458193cb864c09828c85f55b9438277b9e3c7
Jeff <jakerns8379@gmail.com>
2017-03-17 01:06:59 +0000

    remove logic from the route

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 "use strict"
 
-var express = require('express')
-var app = express()
+var express = require('express');
+var ts = require("./services/timestamp.js");
+
+var app = express();
 
 app.get('/', function (req, res) {
   res.send('Timestamp Microservice')
 })
 
+app.get('/:time', (req, res) => {
+  let json = ts(req.params.time);
+  res.json(json);
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
-})
+});
