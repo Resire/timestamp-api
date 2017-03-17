433361c277f9143ae5819251a20497654b5e8861
Jeff <jakerns8379@gmail.com>
2017-03-17 03:54:27 +0000

    output changed for '/' route

--- server.js
+++ server.js
@@ -6,7 +6,22 @@ var ts = require("./services/timestamp.js");
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Timestamp Microservice');
+  let template = `
+    <h1>Timestamp Microservice</h1>
+    <p>User stories:</p>
+    <ol>
+      <li>I can pass a string as a parameter, and it will check to see whether that string contains either a unix timestamp or a natural language date (example: January 1, 2016)</li>
+      <li>If it does, it returns both the Unix timestamp and the natural language form of that date.</li>
+      <li>If it does not contain a date or Unix timestamp, it returns null for those properties.</li>
+    </ol>
+    <h2>Example usage:</h2>
+    <p>https://timestamp-resire.herokuapp.com/December%2015,%202015</p>
+    <p>https://timestamp-resire.herokuapp.com/1450137600</p>
+    <h2>Example output:</h2>
+    <p>{ "unix": 1450137600, "natural": "December 15, 2015" }</p>
+  `;
+  
+  res.send(template);
 });
 
 app.get('/:time', (req, res) => {
